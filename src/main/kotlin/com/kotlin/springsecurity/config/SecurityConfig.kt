cd14d4f6afbb96c7441994bb27f04ecce93177e6
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-27 03:44:12 +0900

    [OAuth, Config] oauth 로그인 서비스를 적용하고, security를 통과할 주소들을 리스트로 따로 정의

--- src/main/kotlin/com/kotlin/springsecurity/config/SecurityConfig.kt
+++ src/main/kotlin/com/kotlin/springsecurity/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.kotlin.springsecurity.config
 
 import com.kotlin.springsecurity.config.filter.JwtTokenFilter
 import com.kotlin.springsecurity.exception.CustomAuthenticationEntryPoint
+import com.kotlin.springsecurity.service.KakaoOAuthUserService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -14,19 +15,28 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 class SecurityConfig(
     private val jwtTokenProvider: JwtTokenProvider
 ) {
+    // 검사를 하지 않는 주소 리스트
+    val whiteList = listOf(
+        "/h2-console",
+        "/h2-console/**",
+        "/v1/user/**",
+        "/auth/login/kakao/**",
+        "/auth/kakao/login",
+        "/images/**",
+        "/"
+    ).toTypedArray()
 
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+    fun securityFilterChain(
+        http: HttpSecurity,
+        kakaoOAuthUserService: KakaoOAuthUserService,
+    ): SecurityFilterChain {
         return http
                 .csrf{ csrf -> csrf.disable() }
-                .headers { it.frameOptions { frame -> frame.disable() } } // h2 콘솔의 ifame 허용
                 .authorizeHttpRequests { auth ->
                     auth
-                        .requestMatchers("/h2-console").permitAll() // h2 콘솔 통과
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/v1/user/login", "/v1/user/regist").permitAll() // 로그인과 회원가입 통과 
-                        .requestMatchers("/v1/main/**").authenticated()
-                        .anyRequest().permitAll() // 모든 인증 통과
+                        .requestMatchers(*whiteList).permitAll() // h2 콘솔 통과
+                        .anyRequest().authenticated() // 모든 인증 검사
                 }
                 .sessionManagement { session -> session
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session 상태 비저장
@@ -35,13 +45,21 @@ class SecurityConfig(
                     JwtTokenFilter(jwtTokenProvider), // jwt 검증 필터
                     UsernamePasswordAuthenticationFilter::class.java // 인증 필터 이전에 jwt 필터 진행
                 )
+                // oauth 로그인 활성화
+                .oauth2Login { oAuth ->
+                    oAuth
+                        // 인증된 사용자 정보를 요청하고 받아오는 엔드포인트 정의
+                        .userInfoEndpoint { userInfo ->
+                            userInfo.userService(kakaoOAuthUserService) // 인증된 사용자 정보를 처리하는 서비스
+                        }
+                }
                 .exceptionHandling { handling ->
                     handling.authenticationEntryPoint(
                         CustomAuthenticationEntryPoint() // 토큰 인증 에러 커스텀
                     )
                 }
+                .headers { it.frameOptions { frame -> frame.disable() } } // h2 콘솔의 ifame 허용
                 .build()
-
     }
 
     @Bean
