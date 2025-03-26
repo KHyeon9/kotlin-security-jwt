e6b498fcc75157413d902fea33a2a93a24ee49d7
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-27 03:39:54 +0900

    [OAuth] oauth 반환시 OAuth2User 형식으로 반환해야 하므로 변환하는 클래스 작성

---
+++ src/main/kotlin/com/kotlin/springsecurity/util/OAuth2UserFromDto.kt
@@ -0,0 +1,25 @@
+package com.kotlin.springsecurity.util
+
+import com.kotlin.springsecurity.dto.user.UserAccountDto
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.core.user.OAuth2User
+
+class OAuth2UserFromDto(
+    private val userAccountDto: UserAccountDto,
+) : OAuth2User {
+    override fun getName(): String {
+        return userAccountDto.userId
+    }
+
+    override fun getAttributes(): MutableMap<String, Any> {
+        return mutableMapOf(
+            "nickname" to userAccountDto.nickname,
+            "role" to userAccountDto.roleType.roleName
+        )
+    }
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return mutableListOf(SimpleGrantedAuthority(userAccountDto.roleType.roleName))
+    }
+}
