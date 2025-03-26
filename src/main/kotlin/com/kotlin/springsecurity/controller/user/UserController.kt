4f49edd2ce6038ec0349ed0be4adecf37ed5a78f
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-03-27 03:36:50 +0900

    [DTO] request, response 들 모두 dto이므로 패키지 변경

--- src/main/kotlin/com/kotlin/springsecurity/controller/user/UserController.kt
+++ src/main/kotlin/com/kotlin/springsecurity/controller/user/UserController.kt
@@ -1,9 +1,9 @@
 package com.kotlin.springsecurity.controller.user
 
-import com.kotlin.springsecurity.controller.request.UserLoginRequest
-import com.kotlin.springsecurity.controller.request.UserRegistRequest
-import com.kotlin.springsecurity.controller.response.UserLoginResponse
-import com.kotlin.springsecurity.controller.response.UserResponse
+import com.kotlin.springsecurity.dto.request.UserLoginRequest
+import com.kotlin.springsecurity.dto.request.UserRegistRequest
+import com.kotlin.springsecurity.dto.response.UserLoginResponse
+import com.kotlin.springsecurity.dto.response.UserResponse
 import com.kotlin.springsecurity.service.UserAccountService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
